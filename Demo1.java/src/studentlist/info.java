e95283ac074e1a44d16797b7b26b7f2fb2e46a68
Admin <Admin@192.168.10.10>
2026-06-01 14:24:46 +0530

    second commit

---
+++ Demo1.java/src/studentlist/info.java
@@ -0,0 +1,30 @@
+package studentlist;
+
+public class info {
+	
+	public void m1() {
+
+		System.out.println("Student name =  Aditi");
+		System.out.println("rollno is = 111");
+		System.out.println("address is = pune ");
+		System.out.println("phone is = 679756656758");
+		
+	}
+	public void m2() {
+
+		System.out.println("Student name =  Rohan");
+		System.out.println("rollno is = 112");
+		System.out.println("address is = pune ");
+		System.out.println("phone is = 6797566567");
+		
+	}
+	
+	public void m3() {
+
+		System.out.println("Student name =  rutu");
+		System.out.println("rollno is = 113");
+		System.out.println("address is = pune,katraj ");
+		System.out.println("phone is = 6797566578");
+	}	
+
+}
